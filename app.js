6de63404da1ecabdbcd13e72e5b57a2f6f823738
Martial NOUNGA <martialnounga@gmail.com>
2023-04-07 17:40:03 +0100

    movies successfully fetched

---
+++ app.js
@@ -0,0 +1,68 @@
+'use strict';
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+const API_KEY = "2623125ab4cda080556c25ecf71a5342";
+
+const genres = {
+    action: 28,
+    aventure: 12,
+    animation: 16,
+    comedie: 35,
+    crime: 80,
+    documentaire: 99,
+    drame: 18,
+    familial: 10751,
+    fantastique: 14,
+    guerre: 10752,
+    histoire: 36,
+    horreur: 27,
+    musique: 10402,
+    mystere: 9648,
+    romance: 10749,
+    amour: 10749,
+    science_fiction: 878,
+    telefilm: 10770,
+    thriller: 53,
+    western: 37
+};
+
+app.post('/api/movies', (req, res, next) => {
+    let category = req.body.queryResult.parameters.genre;
+    console.log(category);
+    category = genres[category];
+    console.log(category);
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=fr&with_genres=${category}`)
+        .then(data => data.json())
+        .then(val => {
+            console.log(val.results);
+            const result = {
+                "fulfillmentMessages": [
+                  {
+                    "card": {
+                      "title": "card title",
+                      "subtitle": "card text",
+                      "imageUri": "https://example.com/images/example.png",
+                      "buttons": [
+                        {
+                          "text": "button text",
+                          "postback": "https://example.com/path/for/end-user/to/follow"
+                        }
+                      ]
+                    }
+                  }
+                ]
+              };
+            res.status(200).json(result);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(400).json({error});
+        })
+});
+
+module.exports = app;
